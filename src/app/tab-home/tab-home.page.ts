adcec343ff188718bca66ec6e608929e55f3636a
tizianomarchetti <27Dicembre16>
2024-01-12 17:17:15 +0100

    notification

---
+++ src/app/tab-home/tab-home.page.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { LocalNotifications } from '@capacitor/local-notifications';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { notificationsOutline } from 'ionicons/icons';
+
+@Component({
+  selector: 'app-tab-home',
+  templateUrl: 'tab-home.page.html',
+  styleUrls: ['tab-home.page.scss'],
+  standalone: true,
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon],
+})
+export class TabHomePage {
+  constructor() {
+    addIcons({ notificationsOutline });
+  }
+
+  async enableNotification() {
+    console.log("abilitando notifiche")
+
+    await LocalNotifications.checkPermissions().then(res => {
+      if (res.display !== 'granted') {
+        LocalNotifications.requestPermissions();
+      }
+    });
+
+    await LocalNotifications.checkPermissions().then(res => {
+      if (res.display === 'granted') {
+        LocalNotifications.schedule({
+          notifications: [
+            {
+              title: "Title",
+              body: "Body",
+              id: 1,
+              schedule: { at: new Date(Date.now() + 1000 * 5) },
+              actionTypeId: "",
+              extra: null
+            }
+          ]
+        });
+      }
+    })
+  }
+}
--- src/app/tabs/tabs.routes.ts
+++ src/app/tabs/tabs.routes.ts
@@ -3,34 +3,19 @@ import { TabsPage } from './tabs.page';
 
 export const routes: Routes = [
   {
-    path: 'tabs',
+    path: '',
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
+        path: 'home',
         loadComponent: () =>
-          import('../tab1/tab1.page').then((m) => m.Tab1Page),
-      },
-      {
-        path: 'tab2',
-        loadComponent: () =>
-          import('../tab2/tab2.page').then((m) => m.Tab2Page),
-      },
-      {
-        path: 'tab3',
-        loadComponent: () =>
-          import('../tab3/tab3.page').then((m) => m.Tab3Page),
+          import('../tab-home/tab-home.page').then((m) => m.TabHomePage),
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/home',
         pathMatch: 'full',
       },
     ],
-  },
-  {
-    path: '',
-    redirectTo: '/tabs/tab1',
-    pathMatch: 'full',
   },
 ];
